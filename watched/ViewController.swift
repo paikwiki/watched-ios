90d6ec8ce13bb8598ba410534cb730002825879c
paikwiki <paikwiki@gmail.com>
2020-05-16 19:55:29 +0900

    Create a project: a Single View App

---
+++ watched/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watched
+//
+//  Created by Changhyun Baek on 2020/05/16.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
